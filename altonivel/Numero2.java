01c23db681c052fa6d507bb591abbc2dfa69df55
João Brentano <joaobrentano.br@gmail.com>
2020-10-15 14:31:42 -0300

    Numero2 altonivel

--- altonivel/Numero2.java
+++ altonivel/Numero2.java
@@ -2,6 +2,20 @@ package altonivel;
 
 public class Numero2 {
     public static void main(String[] args) {
-        
+        // Cria a string para teste
+        String texto = "Isso é uma string abacaxi melancia";
+
+        // Cria um contador de palavras
+        int palavras = 1;
+
+        // Conta as palavras através dos espaços encontrados na frase
+        for(int i = 0; i<texto.length(); i++) {
+            if(texto.charAt(i) == ' ') {
+                palavras++;
+            }
+        }
+
+        // Imprime o resultado da contagem
+        System.out.println("O número de palavras na frase é: " + palavras);
     }
 }
